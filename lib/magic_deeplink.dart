49990331b6bd35a4639770f492ee1be88dd08531
Anilcan Cakir <anilcan.cakir@gmail.com>
2026-02-23 17:14:59 +0300

    refactor: replace barrel export with magic_deeplink.dart

--- lib/magic_deeplink.dart
+++ lib/magic_deeplink.dart
@@ -1,18 +1,16 @@
-library;
+library magic_deeplink;
 
 // Core
 export 'src/deeplink_manager.dart';
 
-// Contracts
-export 'src/handlers/deeplink_handler.dart';
-export 'src/drivers/deeplink_driver.dart';
-
-// Drivers
-export 'src/drivers/app_links_driver.dart';
-
 // Handlers
+export 'src/handlers/deeplink_handler.dart';
 export 'src/handlers/route_deeplink_handler.dart';
 export 'src/handlers/onesignal_deeplink_handler.dart';
+
+// Drivers
+export 'src/drivers/deeplink_driver.dart';
+export 'src/drivers/app_links_driver.dart';
 
 // Providers
 export 'src/providers/deeplink_service_provider.dart';
